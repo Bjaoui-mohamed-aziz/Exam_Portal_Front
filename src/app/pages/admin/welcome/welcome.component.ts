60f198b367630c3e2bcd484458eb1aa111d4c07c
Bjaoui-mohamed-aziz <azizbjaoui312@gmail.com>
2024-10-22 18:49:46 +0100

    finish the implementation of the get test Result API

--- src/app/pages/admin/welcome/welcome.component.ts
+++ src/app/pages/admin/welcome/welcome.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ResultService } from 'src/app/services/result.service';
 
 
 @Component({
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit{
+
+  results = [];
+
+
+  constructor(private _http: HttpClient, private _results : ResultService,
+  ) {}
+
+
+  ngOnInit(): void {
+
+
+    this._results.GetResult().subscribe(
+      (data: any) => {
+        this.results = data;
+        console.log(this.results);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+
+    
+  }
 
 }
--- src/app/services/result.service.ts
+++ src/app/services/result.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import baseUrl from './helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResultService {
 
-  constructor() { }
+
+
+  constructor(private _http : HttpClient) { }
+
+
+  public GetResult()
+  {
+    return this._http.get(`${baseUrl}/question/results`)
+  }
+
 }
